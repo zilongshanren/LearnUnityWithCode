e7478fb58e974270938ba74d627d6845230c60d5
lionqu(屈光辉) <lionqu@tencent.com>
2017-08-26 19:03:10 +0800

    firt commit

---
+++ Assets/IMDrawTriangle.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IMDrawTriangle : MonoBehaviour {
+
+    void OnPostRender()
+    {
+        //GL.PushMatrix();
+        //GL.LoadProjectionMatrix(GetComponent<Camera>().projectionMatrix);
+        //GL.Begin(GL.TRIANGLES);
+        //GL.Color(Color.red);
+        //GL.Vertex(new Vector3(-1, 0, 0));
+        //GL.Color(Color.green);
+        //GL.Vertex(new Vector3(1, 0, 0));
+        //GL.Color(Color.blue);
+        //GL.Vertex(new Vector3(0, 1, 0));
+        //GL.End();
+        //GL.PopMatrix();
+        
+    }
+}
---
+++ Assets/buildMesh.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class buildMesh : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        MeshFilter filter = GetComponent<MeshFilter>();
+        Mesh mesh = filter.mesh;
+
+        Vector3[] vertices = new Vector3[]
+        {
+            new Vector3(-1, -1, 0),
+            new Vector3 (1, -1, 0),
+            new Vector3(0, 1, 0)
+        };
+
+        int[] triangles = new int[3] { 0, 2, 1 };
+
+        Vector2[] uvs = new Vector2[]
+        {
+            new Vector2(0, 0),
+            new Vector2(0.5f, 1.0f),
+            new Vector2(1, 1)
+        };
+
+        Color[] colors = new Color[]
+        {
+            new Color(1, 0, 0, 1),
+            new Color(0, 1, 0, 1),
+            new Color(0, 0, 1, 1)
+        };
+
+        mesh.Clear();
+
+        mesh.vertices = vertices;
+        mesh.uv = uvs;
+        mesh.triangles = triangles;
+        mesh.colors = colors;
+
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+    }
+
+   
+}
